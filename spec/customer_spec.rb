714cafffe4a4979f0b8940d46ad98e5bde72895a
matsushin11 <matsu1983@gmail.com>
2014-12-15 23:59:11 +0900

    Comment out deleted

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -16,21 +16,5 @@ describe Customer do
         expect(customer.statement).to eq "Rental Record for matsuda\n\tTest\t30\nAmount owed is  30\nYou earned 2 frequent renter points"
       }
     end
-=begin
-    context 'sample 1' do
-      let(:programmer_count) { 60 }
-      let(:offers) { [[40, 4300], [30, 2300], [20, 2400]].map{|values| Offer.new(*values)} }
-      specify do
-        expect(Kirishima.main(programmer_count, offers)).to eq 6600
-      end
-    end
-    context 'sample 2' do
-      let(:programmer_count) { 250 }
-      let(:offers) { [[35, 3640], [33, 2706], [98, 9810], [57, 5472], [95, 7790]].map{|values| Offer.new(*values)} }
-      specify do
-        expect(Kirishima.main(programmer_count, offers)).to eq 23072
-      end
-    end
-=end
   end
 end
